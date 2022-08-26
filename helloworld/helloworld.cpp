932235cade4ef328343496e2b6256f17a00277bd
Gaurav <Gaurav@host.docker.internal>
2022-08-26 14:04:19 +0530

    added hello world

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -6,7 +6,7 @@
 // Description : Hello World in C, Ansi-style
 //============================================================================
 
-#include <iostream.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
