e0051db77f3c44783d4348fe5044354f573ca0ad
Gaurav <Gaurav@host.docker.internal>
2022-08-26 13:56:03 +0530

    added c++ hello world project

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : helloworld.cpp
+// Author      : Gaurav
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
